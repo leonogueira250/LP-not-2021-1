be02c3ba5dc52209aa86f7280c6ccd4f22b8d384
leonogueira250 <leonogueira250@gmail.com>
2021-06-09 01:08:13 +0000

    (08/07/2021)

--- Imagens/script.js
+++ Imagens/script.js
@@ -1,5 +1,8 @@
 var menus = document.querySelector(".menus");
 
 menus.addEventListener("click", function(){
+    document.querySelector(".Container").classList.toggle("show-menu");
+});
+menus.addEventListener("click", function(){
     document.querySelector(".sidebar").classList.toggle("show-menu");
 });
